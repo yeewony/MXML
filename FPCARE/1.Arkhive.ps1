f103ddbd9b91d0aa04e9ee10473a9b97e5929df8
yeewony <yeeewony@gmail.com>
2021-03-03 21:35:59 +0900

    210303 1.2. 완성

--- FPCARE/1.Arkhive.ps1
+++ FPCARE/1.Arkhive.ps1
@@ -5,7 +5,7 @@
 
 #시작하기.
 
-$localpath = [String]$PSScriptRoot
+$localpath = $PSScriptRoot
 $global:Err = $null
 $global:foldername = $null
 
@@ -27,17 +27,20 @@ $pdfpath = Get-ChildItem -Path $localpath -Filter *.pdf | % {$_.FullName}
 if($wmvpath -eq $null)
 {
     Write-Host "동영상이 없습니다."
-    $Err = 1
+    Pause
+    exit
 }
 if($resultzippath -eq $null)
 {   Write-Host "점검결과.ZIP 파일이 없습니다." 
-    $Err = 1
+    Pause
+    exit
 }
 
 if($docxpath -eq $null)
 {
     Write-Host "보고서 파일이 없습니다." 
-    $Err = 1
+    Pause
+    exit
 }
 
 
@@ -50,36 +53,34 @@ if($docxpath -eq $null)
 
 #임시 폴더 생성 및 압축 해제
 
-if($Err -ne 1)
-{    
-    mkdir -Path "tmp" | Out-Null
-    Expand-Archive $resultzippath -DestinationPath "$localpath\tmp" | Out-Null
+mkdir -Path "tmp" | Out-Null
+Expand-Archive $resultzippath -DestinationPath "$localpath\tmp" | Out-Null
 
-    #zip 파일 갯수 확인 검사
-    if(!(Get-ChildItem -Path "$localpath\tmp" -Filter *.txt))
-    {
-        Write-Host "점검결과.ZIP 파일 안에 바이러스 점검결과가 없습니다."
-    }
-    if(!(Get-ChildItem -Path "$localpath\tmp" -Filter *After.xml))
-    {
-        Write-Host "점검결과.ZIP 파일 안에 Result_After.xml 파일이 없습니다."
-    }
-    if(!(Get-ChildItem -Path "$localpath\tmp" -Filter *Before.xml))
-    {
-        Write-Host "점검결과.ZIP 파일 안에 Result_Before.xml 파일이 없습니다."
-    }
+#zip 파일 갯수 확인 검사
+if(!(Get-ChildItem -Path "$localpath\tmp" -Filter *.txt))
+{
+    Write-Host "점검결과.ZIP 파일 안에 바이러스 점검결과가 없습니다."
+}
+if(!(Get-ChildItem -Path "$localpath\tmp" -Filter *After.xml))
+{
+    Write-Host "점검결과.ZIP 파일 안에 Result_After.xml 파일이 없습니다."
+}
+if(!(Get-ChildItem -Path "$localpath\tmp" -Filter *Before.xml))
+{
+    Write-Host "점검결과.ZIP 파일 안에 Result_Before.xml 파일이 없습니다."
+}
 
 
-    #Virus_Result.txt 파일 검사
-    $virustxt = Get-Content (Get-ChildItem -Path "$localpath\tmp" -Filter *.txt | % {$_.FullName})
+#Virus_Result.txt 파일 검사
+$virustxt = Get-Content (Get-ChildItem -Path "$localpath\tmp" -Filter *.txt | % {$_.FullName})
 
-    $vRsltbf = ($virustxt | Select-String -Pattern '(?<=점검 대상 객체 :\s).+').Matches.Value.Trim()
-    $vRsltaf = ($virustxt | Select-String -Pattern '(?<=정상 파일 :\s).+').Matches.Value.Trim()
+$vRsltbf = ($virustxt | Select-String -Pattern '(?<=점검 대상 객체 :\s).+').Matches.Value.Trim()
+$vRsltaf = ($virustxt | Select-String -Pattern '(?<=정상 파일 :\s).+').Matches.Value.Trim()
 
-    if($vRsltbf -ne $vRsltaf)
-    {
-        Write-Host "Virus Result 검사 파일 갯수가 다릅니다. Virus_Result_Check.txt 파일을 확인하세요."
-    }
+if($vRsltbf -ne $vRsltaf)
+{
+    Write-Host "Virus Result 검사 파일 갯수가 다릅니다. Virus_Result_Check.txt 파일을 확인하세요."
+}
 
 
 #-----------------------------------------------------------------------------#
@@ -88,28 +89,32 @@ if($Err -ne 1)
 #
 #-----------------------------------------------------------------------------#
 
-    #시간 값 가져오기
-    [xml]$xml = Get-Content (Get-ChildItem -Path "$localpath\tmp" -Filter *Before.xml | % {$_.FullName})
+#시간 값 가져오기
+[xml]$xml = Get-Content (Get-ChildItem -Path "$localpath\tmp" -Filter *Before.xml | % {$_.FullName})
 
-    if(([int]$foldername = $xml.'PC-Check'.START_TIME.Split()[1].Split(":")[0]) -eq 0)
-    {
-        [int]$foldername = $xml.'PC-Check'.START_TIME.Split()[2].Split(":")[0]
-    }
+[int]$foldername = $xml.'PC-Check'.START_TIME.Split()[1].Split(":")[0]
 
-    if($foldername -lt 10)
-    {
-        [string]$foldername = "0"+[string]$foldername
-    }
-    else
-    {
-        [string]$foldername = [string]$foldername
-    }
+if($foldername -eq 0)
+{
+    [int]$foldername = $xml.'PC-Check'.START_TIME.Split()[2].Split(":")[0]
+}
 
-    #시간 폴더 생성
-    mkdir -Path "$localpath\$foldername" | Out-Null
+if($foldername -lt 10)
+{
+    [string]$foldername = "0"+[string]$foldername
+}
+else
+{
+    [string]$foldername = [string]$foldername
+}
 
-    #임시폴더 삭제
-    rmdir -Path "$localpath\tmp" -Force -Recurse | Out-Null
+$foldername
+
+#시간 폴더 생성
+mkdir -Path "$localpath\$foldername" | Out-Null
+
+#임시폴더 삭제
+rmdir -Path "$localpath\tmp" -Force -Recurse | Out-Null
 
 
 #-----------------------------------------------------------------------------#
@@ -117,16 +122,10 @@ if($Err -ne 1)
 #                               파일 옮기기
 #
 #-----------------------------------------------------------------------------#
-  
-    Move-Item -Path $wmvpath -Destination "$localpath\$foldername"
-    Move-Item -Path $resultzippath -Destination "$localpath\$foldername"
-    Move-Item -Path $datazippath -Destination "$localpath\$foldername"
-    Move-Item -Path $docxpath -Destination "$localpath\$foldername"
-    Move-Item -Path $pdfpath -Destination "$localpath\$foldername" 
 
-}
+Move-Item -Path $wmvpath -Destination "$localpath\$foldername" | Out-Null
+Move-Item -Path $resultzippath -Destination "$localpath\$foldername" | Out-Null
+Move-Item -Path $datazippath -Destination "$localpath\$foldername" | Out-Null
+Move-Item -Path $docxpath -Destination "$localpath\$foldername" | Out-Null
+Move-Item -Path $pdfpath -Destination "$localpath\$foldername" | Out-Null
 
-if($err -eq 1) {
-    Write-Host "오류를 확인하세요"
-    Pause
-}
