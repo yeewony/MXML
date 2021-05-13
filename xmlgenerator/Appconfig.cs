a97c4fe1db6fee6cc289d2f9085583f01c750e00
yeewony <yeeewony@gmail.com>
2021-05-13 17:10:57 +0900

    XML 에디터 분리 작업

---
+++ xmlgenerator/Appconfig.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+///아래 참조 영역에 추가
+using System.Configuration;
+
+namespace xmlgenerator
+{
+    class Appconfig
+    {
+        ///Summary///
+        ///Config.ini 파일을 만들고 해당 컨픽을 불러온다.///
+        ///프로젝트 참조에 System.Configuration DLL을 추가하고 클래스에 System.Configuration 참조를 추가한다.
+        ///레퍼런스에 System.Configuration 추가
+        ///
+        
+        //추가하기 SetConfig("key" , "value");
+        public static void SetConfig(string key, string val)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            KeyValueConfigurationCollection configCollection = config.AppSettings.Settings;
+
+            configCollection.Add(key, val);
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+
+        }
+
+        //가져오기 string cfg = GetConfig("key");
+        public static string GetConfig(string key)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            KeyValueConfigurationCollection configcollection = config.AppSettings.Settings;
+
+            string val = "";
+            try
+            {
+                val = configcollection[key].Value;
+            }
+            catch { }
+
+            return val;
+        }
+
+        //삭제하기 DelConfig("KEY");
+        public static void DelConfig(string key)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            KeyValueConfigurationCollection configcollection = config.AppSettings.Settings;
+
+            try
+            {
+                configcollection.Remove(key);
+
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+            }
+            catch { }
+        }
+
+    }
+}
---
+++ xmlgenerator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace xmlgenerator
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MobileXML.Generate("22:22 연월일", "10", "SM_048d", "SAMSUNG", "SKT");
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            string[] File = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (File != null && File.Length != 0)
+            {
+                testbox.Text = File[0];
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string zip = testbox.Text;
+            LogExtractor.LogToXML(zip);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
