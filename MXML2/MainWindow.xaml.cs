9b74d425708653737020a85f8b72b979b40cbdc5
yeewony <yeeewony@gmail.com>
2021-05-29 07:42:01 +0900

    mxml2 making..

---
+++ MXML2/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MXML2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DetectColorMode(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(Environment.CurrentDirectory+"\\Pink_MXML2.exe"))
+            {
+                MessageBox.Show("핑크로 전환합니다.");
+            }
+        }
+
+        private void START_Inspection(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                MessageBox.Show("점검 전 방해금지 모드 설정");
+
+
+                tb_StartTime.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void END_Inspection(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                tb_EndTime.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void GenerateXML(object sender, RoutedEventArgs e)
+        {
+            MobileXML.GenerateXML(tb_StartTime.Text, tb_EndTime.Text, tb_OSVer.Text, tb_ModelName.Text, tb_Manufacturer.Text, tb_Carrier.Text);
+
+        }
+    }
+}
---
+++ MXML2/MobileXML.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Xml;
+using System.IO;
+
+namespace MXML2
+{
+    class MobileXML
+    {
+        public static void GenerateXML(string StartTime, string EndTime, string OSVer, string ModelName, string Manufacturer, string Carrier)
+        {
+            try
+            {
+                XmlDocument Mxml = new XmlDocument();
+
+                XmlDeclaration Mxmldecl;
+                Mxmldecl = Mxml.CreateXmlDeclaration("1.0", "UTF8", null);
+                Mxmldecl.Encoding = "UTF-8";
+                Mxmldecl.Standalone = "yes";
+
+                XmlNode root = Mxml.CreateElement("MOBILE-Check");
+                Mxml.AppendChild(root);
+
+                XmlNode START_TIME = Mxml.CreateElement("START_TIME");
+                START_TIME.InnerText = StartTime;
+                root.AppendChild(START_TIME);
+
+                XmlNode END_TIME = Mxml.CreateElement("END_TIME");
+                END_TIME.InnerText = EndTime;
+                root.AppendChild(END_TIME);
+
+                XmlNode ANDROID_VERSION = Mxml.CreateElement("ANDROID_VERSION");
+                ANDROID_VERSION.InnerText = OSVer;
+                root.AppendChild(ANDROID_VERSION);
+
+                XmlNode MODEL_VERSION = Mxml.CreateElement("MODEL_VERSION");
+                MODEL_VERSION.InnerText = ModelName;
+                root.AppendChild(MODEL_VERSION);
+
+                XmlNode MANUFACTURER = Mxml.CreateElement("MANUFACTURER");
+                MANUFACTURER.InnerText = Manufacturer;
+                root.AppendChild(MANUFACTURER);
+
+                XmlNode CARRIER = Mxml.CreateElement("CARRIER");
+                CARRIER.InnerText = Carrier;
+                root.AppendChild(CARRIER);
+
+                for (int i = 1; i <= 7; i++)
+                {
+                    string monum = "MO-0" + i;
+
+                    XmlNode mo = Mxml.CreateElement(monum);
+                    XmlNode NAME = Mxml.CreateElement("NAME"); NAME.InnerText = ""; mo.AppendChild(NAME);
+                    XmlNode RESULT = Mxml.CreateElement("RESULT"); RESULT.InnerText = ""; mo.AppendChild(RESULT);
+                    root.AppendChild(mo);
+                }
+
+                string bfxmlfile = Environment.CurrentDirectory + "\\MOBILE_" + ModelName + "_Result_Before.xml";
+                string afxmlfile = Environment.CurrentDirectory + "\\MOBILE_" + ModelName + "_Result_After.xml";
+
+                if (File.Exists(bfxmlfile))
+                {
+                    if (File.Exists(afxmlfile))
+                    {
+                        File.Delete(afxmlfile);
+                    }
+                    File.Delete(bfxmlfile);
+                }
+
+                Mxml.Save(bfxmlfile);
+                Mxml.Save(afxmlfile);
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public static void ParsingClipData(string ClipData)
+        {
+
+        }
+    }
+}
